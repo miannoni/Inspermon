b7b422a15ae62bd27e58475354e7af6ffb50da11
PhelipeMuller <Phelipe@navesmuller.com.br>
2017-03-30 00:02:17 -0300

    Add files via upload
    Arquivo NOVO
    (Possível substituto do mons.py)
    Possui os InsperDex já em uma Lista, que possui, Nome, Tipo e outros dados para a criação das Fichas de Monstro
    9 Monstros Iniciais, já listados

---
+++ insperdex.py
@@ -0,0 +1,13 @@
+insperdex = [0]*9
+#Nome, Tipo, AtqI, AtqM, AtqEI, AtqEM, DefI, DefM, DefEI, DefEM, HPi, HPm, Veli, Velm, nvEVO
+# 0		1	   2	3 	   4	   5	6	   7 	  8		9	  10   11    12	   13    14
+
+insperdex[0] =["Bulbasauro", "Planta", 6, 1.29, 6, 1.61, 6, 1.29, 6, 1.61, 12, 2.21, 6, 1.21, 16]
+insperdex[1] =["Ivysaur", "Planta", 6, 1.55, 6, 1.91, 6, 1.57, 6, 1.91, 12, 2.51, 6, 1.51, 36]
+insperdex[2] =["Venusaur", "Planta", 6, 1.95, 7, 2.31, 6, 1.97, 7, 2.31, 12, 2.91, 6, 1.91, 101]
+insperdex[3] =["Charmander", "Fogo", 6, 1.35, 6, 1.51, 6, 1.17, 6, 1.61, 12, 2.09, 6, 1.61, 16]
+insperdex[4] =["Charmeleon", "Fogo", 6, 1.59, 6, 1.91, 6, 1.47, 6, 1.61, 12, 2.47, 6, 1.91, 36]
+insperdex[5] =["Charizard", "Fogo", 6, 2, 7, 2.49, 6, 1.87, 7, 2.01, 12, 2.87, 7, 2.31, 101]
+insperdex[6] =["Squirtle", "Agua", 6, 1.27, 6, 1.31, 6, 1.61, 6, 1.59, 12, 2.19, 6, 1.17, 16]
+insperdex[7] =["Wartortle", "Agua", 6, 1.57, 6, 1.61, 6, 1.91, 6, 1.91, 12, 2.49, 6, 1.47, 36]
+insperdex[8] =["Blastoise", "Agua", 6, 1.97, 7, 2.091, 7, 2.31, 7, 2.41, 12, 2.89, 6, 1.87, 101]
