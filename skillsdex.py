4f3a52b55fa558f656915f5f75b370c1a9433239
PhelipeMuller <Phelipe@navesmuller.com.br>
2017-03-30 20:13:02 -0300

    Create skillsdex.py
    Inicio do Dex de Habilidades

---
+++ skillsdex.py
@@ -0,0 +1,9 @@
+#Nome, Tipo, Força, N0/E1, speed(0a20), PPmax
+#  0	1	   2	 3			4		  5
+def ReturnSkilldex()
+	skilldex = [0]*4
+	skilldex[0] = ["Soco", "Normal", 50, 0, 10, 25]
+	skilldex[1] = ["Atear Fogo","Fogo", 50, 1, 10, 25]
+	skilldex[2] = ["Bolhas","Agua",50, 1, 10, 25]
+	skilldex[3] = ["Disparar Sementes", "Planta", 50, 1, 10, 25]
+	return skilldex
